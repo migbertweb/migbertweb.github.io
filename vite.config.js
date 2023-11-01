ea045dca874a8ee43362c65b73025489b0e88837
migbertweb <migbertyanez@disroot.org>
2023-11-01 16:20:06 -0300

    base update

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://migbertweb.github.io/migbertweb.github.io/",
+  base: "https://migbertweb.github.io",
 });
